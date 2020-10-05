0ce3ff816f3bcf40769a1a75a01c036212e64508
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-10-05 12:42:12 -0300

    Cap02. Item 1. Add other comments

--- src/cap01/Item01.java
+++ src/cap01/Item01.java
@@ -5,16 +5,38 @@ package cap01;
  * 
  * 1) Ao contrário dos construtores, possuem nomes.
  * 
- * 2) Nao precisam retornar um novo objeto sempre que invocados.
+ * 2) Nao precisam retornar um novo objeto sempre que invocados (performance
+ * e/ou maior controle sobre as instancias existentes).
+ * 
+ * 3) Podem retornar um objeto de qualquer subtipo, dando grande flexibilidade
+ * na escolha do objeto retornado (mantendo-as privadas por exemplo).
+ * 
+ * 4) Podem variar o objeto de retorno de chamada para chamada. Ex: EnumSet
+ * retorna instancias diferentes dependendo da quantidade de elementos.
+ * 
+ * 5) A classe do objeto retornado pode ser criada depois do static factory (Ex:
+ * service provider frameworks: API de acesso ao serviço)
+ * 
+ * Alguns nomes utilizados:
+ * 
+ * from: Data.from(instant)
+ * 
+ * of: EnumSet.of(X,Y,Z)
+ * 
+ * valueOf: BigInteger.valueOf(Integer.MAX_VALUE)
+ * 
+ * intance/getIntance: StackWalker.getInstance(options)
+ * 
+ * create/newInstance: Array.newInstance(...)
+ * 
+ * getType: Files.getFileStore(path)
+ * 
+ * newType: Files.newBufferedReader(path)
+ * 
+ * type: Collections.list(...)
+ * 
  */
 public class Item01 {
-    public static Item01 algumNomeSignificativo() {
-        return new Item01();
-    }
-
-    public static Item01 algumNomeSignificativoQueRecebeParametro(String nome) {
-        return new Item01(nome);
-    }
 
     private Item01() {
     }
@@ -23,9 +45,20 @@ public class Item01 {
         // ...
     }
 
-    private static Item01 item01 = new Item01();;
+    // 01:
+    public static Item01 algumNomeSignificativo() {
+        return new Item01();
+    }
+
+    public static Item01 algumNomeSignificativoQueRecebeParametro(String nome) {
+        return new Item01(nome);
+    }
+
+    // 02:
+    private static Item01 item01 = new Item01();
 
     public static Item01 naoRetornaNovoObjeto() {
         return item01;
     }
+
 }
